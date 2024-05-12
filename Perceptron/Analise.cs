f7b3d5ba701a8051a30c0986ff83a78b94188d95
Pedro Castro <30752706+castroallves@users.noreply.github.com>
2024-05-12 20:06:08 -0300

    Add files via upload

---
+++ Perceptron/Analise.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ReconhecimentoDeLetras
+{
+    class Analise
+    {
+
+        // Padrão para a letra T
+        public int[] letra_t = new int[25] { 1, 1, 1, 1, 1,
+                                             0, 0, 1, 0, 0,
+                                             0, 0, 1, 0, 0,
+                                             0, 0, 1, 0, 0,
+                                             0, 0, 1, 0, 0 };
+
+        // Padrão para a letra X								
+        public int[,] letra_n = new int[2, 25] {{ 1, 0, 0, 0, 1,
+                                                  0, 1, 0, 1, 0,
+                                                  0, 0, 1, 0, 0,
+                                                  0, 1, 0, 1, 0,
+                                                  1, 0, 0, 0, 1 },
+
+                                                { 1, 1, 1, 1, 1,
+                                                  0, 0, 1, 0, 0,
+                                                  0, 0, 1, 0, 0,
+                                                  0, 0, 1, 0, 0,
+                                                  0, 0, 1, 0, 0 }};
+    public string info_analises = null;
+        public float[] wantigo = new float[25];
+        public float[] wnovo = new float[25];
+        public int t;
+        public float y;
+                
+        int[,] x = new int[4, 2];
+        public float bnovo = 0, bantigo = 0, theta = 0,// limiar
+                                          trocou = 1,// sinaliza mudança dos pesos
+                                          ciclos = 0; // contador para número de ciclos de treinamento
+                                          
+        public float alpha = 1, yliquido = 0;// taxa de aprendizagem
+
+        void inicializa() {                           
+            for (int i = 0; i < 25; i++) {
+                wantigo[i] = 0;
+                wnovo[i] = 0;
+                info_analises += "W de " + i.ToString() + "= " + wnovo[i].ToString() +
+                                 "  B =  " + bnovo.ToString() + "\n";
+                //if(i ==  10 ) info_analises += "\t"                ;
+            }
+        }
+
+        public void treina(int numero)
+        {
+            if (numero == 0) { t = 1; } else { t = -1; }
+            inicializa();
+            while(trocou == 1)
+            {
+                MessageBox.Show("Ciclo " + ciclos.ToString() + "\n\n" + info_analises);
+                trocou = 0;
+                ciclos++;
+                for(int i = 0; i < 25; i++)
+                {
+                    yliquido += wantigo[i] * letra_n[numero,i] + bantigo;
+                    
+                }
+
+                if (yliquido >= theta)
+                { y = 1; }
+                else { y = -1; }
+
+                // atualiza pesos se rede neural errou
+                if (y != t)
+                {
+                    info_analises = null;
+                    trocou = 1;  //  sinaliza que a rede neural errou
+                    for (int n = 0; n < 25; n++)
+                    {
+                        wnovo[n] = wantigo[n] + alpha * letra_n[numero,n] * t;
+                        info_analises += "W de " + n.ToString() + "= " + wnovo[n].ToString() +
+                                 " B =  " + bnovo.ToString() + "\n";
+                    }
+                    bnovo = bantigo + alpha * t;
+                    // salva os novos pesos
+                    wantigo = wnovo;
+                    bantigo = bnovo;
+                }
+
+
+                //MessageBox.Show(info_analises);
+                
+            }
+
+            MessageBox.Show("A saída é: " + y.ToString());
+        }
+}
+
+}
---
+++ Perceptron/Form1.Designer.cs
@@ -0,0 +1,77 @@
+﻿
+namespace ReconhecimentoDeLetras
+{
+    partial class Perceptrons
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.bt_X = new System.Windows.Forms.Button();
+            this.bt_T = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // bt_X
+            // 
+            this.bt_X.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_X.Location = new System.Drawing.Point(31, 51);
+            this.bt_X.Name = "bt_X";
+            this.bt_X.Size = new System.Drawing.Size(80, 74);
+            this.bt_X.TabIndex = 5;
+            this.bt_X.Text = "X";
+            this.bt_X.UseVisualStyleBackColor = true;
+            this.bt_X.Click += new System.EventHandler(this.bt_X_Click);
+            // 
+            // bt_T
+            // 
+            this.bt_T.Font = new System.Drawing.Font("Microsoft Sans Serif", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bt_T.Location = new System.Drawing.Point(138, 51);
+            this.bt_T.Name = "bt_T";
+            this.bt_T.Size = new System.Drawing.Size(78, 74);
+            this.bt_T.TabIndex = 6;
+            this.bt_T.Text = "T";
+            this.bt_T.UseVisualStyleBackColor = true;
+            this.bt_T.Click += new System.EventHandler(this.bt_T_Click);
+            // 
+            // Perceptrons
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.AliceBlue;
+            this.ClientSize = new System.Drawing.Size(259, 199);
+            this.Controls.Add(this.bt_T);
+            this.Controls.Add(this.bt_X);
+            this.Name = "Perceptrons";
+            this.Text = "Perceptrons";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button bt_X;
+        private System.Windows.Forms.Button bt_T;
+    }
+}
+
